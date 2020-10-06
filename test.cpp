7b38254a8e52fc2b5c6b8719f594f59ee5b10a3b
alliyahmunir <amuni012@ucr.edu>
2020-10-05 19:29:40 -0700

    Lab 1 Section 26

--- test.cpp
+++ test.cpp
@@ -13,20 +13,19 @@ TEST(EchoTest, EmptyString) {
 }
 
 
-#My Test1 uses long string, by displaying a long Disney Greeting
+//My Test1 uses long string, by displaying a long Disney Greeting
 TEST(EchoTest, DisneyGreeting) {
-    long string* test_val[10]; test_val[0] = "./c-echo"; 
+    char* test_val[9]; test_val[0] = "./c-echo"; 
     test_val[1] = "Welcome"; test_val[2] = "Back";
     test_val[3] = "to"; test_val[4] = "Disney,"; 
-    test_val[5] = "Where"; test_val[6] = "Your";
-    test_val[7] = "Dreams"; test_val[8] = "Come";
-    test_val[9] = "True!";
+    test_val[5] = "Where"; test_val[6] = "Dreams";
+    test_val[7] = "Come";test_val[8] = "True!";
 
-    EXPECT_EQ("Welcome Back to Disney, Where Your Dreams Come True!", echo(10,test_val));
+    EXPECT_EQ("Welcome Back to Disney, Where Dreams Come True!", echo(9,test_val));
 }
 
 
-#My Test2 uses int to display a special character(!) and a number countdown
+//My Test2 uses int to display a special character(!) and a number countdown
 TEST(EchoTest, CountDown) {
     char* test_val[6]; test_val[0] = "./c-echo"; 
     test_val[1] = "5!"; test_val[2] = "4!";
@@ -34,10 +33,10 @@ TEST(EchoTest, CountDown) {
     EXPECT_EQ("5! 4! 3! 2! 1!", echo(6,test_val));
 }
 
-#My Test3 uses long char to display the first 4 alphabet letters
-#testing uppercase, lowercase, and special character feature
+//My Test3 uses long char to display the first 4 alphabet letters
+//testing uppercase, lowercase, and special character feature
 TEST(EchoTest, Alphabet) {
-    long char* test_val[9]; test_val[0] = "./c-echo"; 
+    char* test_val[9]; test_val[0] = "./c-echo"; 
     test_val[1] = "A,"; test_val[2] = "B,";
     test_val[3] = "C,"; test_val[4] = "D,";
     test_val[5] = "a,"; test_val[6] = "b,";
